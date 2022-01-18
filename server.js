cb445e47dceda77ce128c88ff79c10c042e2ec51
Nilay barde <nilaybarde@Nilays-MacBook-Pro.local>
2022-01-18 18:49:45 -0500

    Fixing heroku deployment with port listening change

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/hello', (req, res) =>
   res.send('Hello World!'));
 
 const PORT = 4000;
-app.listen(PORT);
+app.listen(process.env.PORT || PORT, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// app.listen(PORT);
