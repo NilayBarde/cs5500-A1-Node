98a42af4a7e723488b2250d63b95fdcd7a1df67f
nilaybarde <nilaybarde@Nilays-MacBook-Pro.local>
2022-01-24 11:56:20 -0500

    Add updates based on changes made to repo

--- server.ts
+++ server.ts
@@ -4,8 +4,9 @@ const app = express();
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
 
-const PORT = 4000;
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+})
 
-app.listen(process.env.PORT, () => {
-    console.log(`server started`)
-});
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
