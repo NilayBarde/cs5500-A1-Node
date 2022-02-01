fd3813ea268f860dc11b315d6adbe4dd2fdebbf6
nilaybarde <nilaybarde@Nilays-MacBook-Pro.local>
2022-02-01 18:23:09 -0500

    Add all object design patterns

---
+++ interfaces/UserDao.ts
@@ -0,0 +1,9 @@
+import User from "../models/User";
+
+export default interface UserDao {
+   findAllUsers(): Promise<User[]>;
+   findUserById(userId: string): Promise<User>;
+   createUser(user: User): Promise<User>;
+   updateUser(userId: string, user: User): Promise<any>;
+   deleteUser(userId: string): Promise<any>;
+}
