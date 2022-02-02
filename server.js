b5f4f824bc5d2f839b33ac481ab8b4a11d404042
Nilay Barde <nilaybarde@gmail.com>
2022-02-02 00:36:33 -0500

    movies db complete

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/movie-db");
+mongoose.connect("mongodb://0.0.0.0:27017/movie-db");
 
 app.get("/hello", (req, res) => res.send("Hello World!"));
 
-const PORT = 4000;
 require("./movies/service")(app);
+
+const PORT = 4000;
 
 app.listen(PORT);
